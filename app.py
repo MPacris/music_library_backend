fe78cfa5529ed3f79312d40af320ef0ad26abcf2
MPacris <mpacris@yahoo.com>
2023-04-24 13:29:05 -0500

    added in schema

--- app.py
+++ app.py
@@ -38,7 +38,23 @@ class Song(db.Model):
 
 # Schemas
 
+class SongSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    title = fields.String(required=True)
+    artist = fields.String(required=True)
+    album = fields.String()
+    release_date = fields.Date()
+    genre = fields.String()
 
+    class Meta:
+        fields = ("id", "title", "artist", "album", "release_date", "genre")
+
+    @post_load
+    def create_song(self, data, **kwargs):
+        return Song(**data)
+
+song_schema = SongSchema()
+songs_schema = SongSchema(many=True)
 
 # Resources
 
